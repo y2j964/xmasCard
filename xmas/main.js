63e426c195565274aea759fc01e6d9c20e86bee0
Justin Mooney <34282795+y2j964@users.noreply.github.com>
2018-12-20 01:00:32 -0500

    Update main.js

--- xmas/main.js
+++ xmas/main.js
@@ -1,10 +1,30 @@
 let button = document.querySelector("button");
 let greatestSong = document.querySelector(".videoHidden");
 let vimeoPlayer = document.querySelector("iframe");
+let songOption = document.querySelector("select");
 
+
+songOption.addEventListener("change", function(e) {
+  greatestSong.className = "videoHidden";
+  switch (e.target.value) {
+    case "sail":
+      vimeoPlayer.src = "https://player.vimeo.com/video/170928574";
+      break;
+    case "surf":
+      vimeoPlayer.src = "https://www.dailymotion.com/embed/video/xu6v84";
+      break;
+    case "summer":
+      vimeoPlayer.src = "https://www.dailymotion.com/embed/video/xu6v71" ;
+      break;
+    default:
+      vimeoPlayer.src= "";
+  }  
+})
 
 button.addEventListener("click", function() {
-  greatestSong.className = "videoDisplay";
-  // greatestSong.autoplay = "autoplay";
-  vimeoPlayer.src += "?autoplay=1";
-})
+  const srcFilePath = "file:///C:/Users/JCM/Documents/webpages/web1/xmas/index1.html";
+  if (vimeoPlayer.src !== srcFilePath) {   
+    greatestSong.className = "videoDisplay";
+    vimeoPlayer.src += "?autoplay=1";
+  } 
+}) 
