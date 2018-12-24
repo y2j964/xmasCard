8e98c21276766cc30f012b4df2ddcc6f7595ddda
Justin Mooney <34282795+y2j964@users.noreply.github.com>
2018-12-24 01:24:23 -0500

    Update main.js

--- xmas/main.js
+++ xmas/main.js
@@ -2,8 +2,23 @@ let button = document.querySelector("button");
 let greatestSong = document.querySelector(".videoHidden");
 let vimeoPlayer = document.querySelector("iframe");
 let songOption = document.querySelector("select");
+let barToggler = document.querySelector("i");
+let navbarLinks = Array.from(document.getElementsByTagName("li"));
+navbarLinks = navbarLinks.slice(1);
 
-
+barToggler = addEventListener("click", function() {
+  // console.log(navbarLinks[0].className)
+  for (let i = 0; i < navbarLinks.length; i++) {
+    if (navbarLinks[i].className === "navLinkProper") {
+      navbarLinks[i].className = "expandedNav";
+      // console.log("link " + i + " now expanded")
+    } else {
+      navbarLinks[i].className = "navLinkProper";
+      // console.log("link " + i + " now retracted")      
+    }
+  }
+  })
+  
 songOption.addEventListener("change", function(e) {
   greatestSong.className = "videoHidden";
   switch (e.target.value) {
@@ -22,7 +37,7 @@ songOption.addEventListener("change", function(e) {
 })
 
 button.addEventListener("click", function() {
-  const srcFilePath = "https://y2j964.github.io/xmasCard/xmas/";
+  const srcFilePath = "file:///C:/Users/JCM/Documents/webpages/web1/xmas/index1.html";
   if (vimeoPlayer.src !== srcFilePath) {   
     greatestSong.className = "videoDisplay";
     vimeoPlayer.src += "?autoplay=1";
